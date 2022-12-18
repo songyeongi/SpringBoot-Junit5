d47ed380f69a46117c72b7267e8d1a35c776e7c0
ID_GyeongI <>
2022-12-18 23:56:03 +0900

    controller 저장하기 메서드 단위테스트, 통합테스트

--- src/test/java/com/cos/book/web/BookControllerIntegreTest.java
+++ src/test/java/com/cos/book/web/BookControllerIntegreTest.java
@@ -1,10 +1,22 @@
 package com.cos.book.web;
 
+import com.cos.book.domain.Book;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * 통합 테스트 (모든 Bean들을 똑같이 IoC에 올리고 테스트 하는 것)
@@ -23,6 +35,7 @@ import org.springframework.transaction.annotation.Transactional;
  * @Transactional은 각각의 테스트함수가 종료될 때마다 트랜잭션을 rollback해주는 어노테이션 // 독립적으로 테스트 가능
  */
 
+@Slf4j
 @Transactional
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc // 이걸 안 넣으면 MockMvc dependency injection을 못한다. (IoC에 없다는 뜻)
@@ -35,4 +48,32 @@ public class BookControllerIntegreTest {
     //mockito라고 하는 라이브러리가 하는 역할 mockMvc.perform에서 mockito로
     // controller의 주소로 테스트 해 볼 수 있는 라이브러리
     // mockMvc를 통해 테스트할 거다
+
+    @Test
+    public void save_테스트() throws Exception {
+        // given (테스트를 하기 위한 준비)
+        Book book = new Book(null, "스프링 따라하기", "코스");
+        String content = new ObjectMapper().writeValueAsString(book); // Object를 json으로 바꾸는 함수
+        // json 파싱해서 string으로 보여준다.
+
+        // 단위테스트 작성 후 통합테스트에 옮겨도 된다.
+        // stub만들기 가정법 미리 행동지정
+        // 통합 테스트는 실제 서비스가 메모리에 뜬다. 실제 서비스가 실행되니깐 stub이 필요없다.
+//        when(bookService.저장하기(book)).thenReturn(new Book(1L, "스프링 따라하기", "코스"));
+        // stub이 필요없다는 건 가짜 @MockBean bookservice가 필요없다. 진짜가 있으니깐
+        // 차이는 통합은 전체를 테스트하니깐 bean이 다 뜬다. @WebMvcTest는 컨트롤러만 띄우니깐 가볍다. 통합은 무거움
+        // 통합테스트는 쉽다 근데 디버깅하기 어렵다. validation, security, filter들이 다 들어가면 검증하기 어렵다.
+
+        // when (테스트 실행)
+        ResultActions resultActions = mockMvc.perform(post("/book")
+                .contentType(MediaType.APPLICATION_JSON) //지금 내가 던지는 데이터가 뭔지 알려주기
+                .content(content) // 실제로 던질 데이터
+                .accept(MediaType.APPLICATION_JSON)); //응답은 똑같이 json
+
+        // then (검증)
+        resultActions
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title").value("스프링 따라하기")) // json의 데이터를 return 할거다 그 때 쓰는 jsonPath 함수
+                .andDo(MockMvcResultHandlers.print()); // 결과를 console에 보여준다.
+    }
 }
--- src/test/java/com/cos/book/web/BookControllerUnitTest.java
+++ src/test/java/com/cos/book/web/BookControllerUnitTest.java
@@ -1,6 +1,24 @@
 package com.cos.book.web;
 
+import com.cos.book.domain.Book;
+import com.cos.book.service.BookService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * 단위 테스트 (Controller 관련 로직만 띄우기) Controller만 분리해서 테스트
@@ -14,6 +32,38 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
  * 스프링에서 junit 테스트를 할 때 스프링으로 확장해줘야 한다. @ExtendWith(SpringExtension.class)는 필수
  */
 
+@Slf4j
 @WebMvcTest
 public class BookControllerUnitTest {
+    @Autowired
+    private MockMvc mockMvc; // 주소 호출해서 테스트 해주는 도구 @WebMvcTest안에 @AutoConfigureMockMvc가 있다.
+
+    // @Mock // Mock은 mockito 환경에 메모리가 뜬다. 가짜
+    @MockBean // BookService가 IoC 환경에 bean으로 뜽록됨 / 가짜 서비스 실제로 동작 제대로 안함 / 스프링 환경
+    // 가짜 bookservice 객체를 IoC에 등록
+    private BookService bookService;
+
+    // BDDMockito 패턴 given, when, then
+    @Test
+    public void save_테스트() throws Exception {
+        // given (테스트를 하기 위한 준비)
+        Book book = new Book(null, "스프링 따라하기", "코스");
+        String content = new ObjectMapper().writeValueAsString(book); // Object를 json으로 바꾸는 함수
+        // json 파싱해서 string으로 보여준다.
+
+        // stub만들기 가정법 미리 행동지정
+        when(bookService.저장하기(book)).thenReturn(new Book(1L, "스프링 따라하기", "코스"));
+
+        // when (테스트 실행)
+        ResultActions resultActions = mockMvc.perform(post("/book")
+                .contentType(MediaType.APPLICATION_JSON) //지금 내가 던지는 데이터가 뭔지 알려주기
+                .content(content) // 실제로 던질 데이터
+                .accept(MediaType.APPLICATION_JSON)); //응답은 똑같이 json
+
+        // then (검증)
+        resultActions
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title").value("스프링 따라하기")) // json의 데이터를 return 할거다 그 때 쓰는 jsonPath 함수
+                .andDo(MockMvcResultHandlers.print()); // 결과를 console에 보여준다.
+    }
 }
